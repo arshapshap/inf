111d01f3594fd3e9ad581be25da4dd24d21ceb17
Artyom Shapovalov <arshapshap@gmail.com>
2022-11-05 19:44:26 +0300

    [add] week8

---
+++ week8/ORM/AccountDAO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer
+{
+    internal class AccountDAO
+    {
+        readonly MyORM orm;
+
+        public AccountDAO(string connectionString, string tableName)
+        {
+            orm = new MyORM(connectionString, tableName);
+        }
+
+        public Account[] Select() => orm.Select<Account>();
+
+        public Account? Select(int id) => orm.Select<Account>(id);
+
+        public void Insert(Account account) => orm.Insert(account);
+
+        public void Update(int id, Account account) => orm.Update(id, account);
+
+        public void Delete(int id) => orm.Delete(id);
+    }
+}
---
+++ week8/ORM/MyORM.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer
+{
+    internal class MyORM
+    {
+        public string TableName;
+        readonly string connectionString;
+
+        public MyORM(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public MyORM(string connectionString, string tableName) : this(connectionString)
+        {
+            TableName = tableName;
+        }
+
+        public T[] Select<T>()
+        {
+            var result = new List<T>();
+
+            string sqlExpression = $"SELECT * FROM [dbo].[{TableName}]";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        var newItem = Activator.CreateInstance(typeof(T), GetValues(reader));
+                        if (newItem is T item)
+                            result.Add(item);
+                    }
+                }
+
+                reader.Close();
+            }
+            return result.ToArray();
+        }
+
+        public T? Select<T>(int id)
+        {
+            T? result = default;
+            string sqlExpression = $"SELECT * FROM [dbo].[{TableName}] WHERE id = {id}";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows && reader.Read())
+                {
+                    result = (T?)Activator.CreateInstance(typeof(T), GetValues(reader));
+                }
+
+                reader.Close();
+            }
+            return result;
+        }
+
+        public void Insert<T>(T item)
+        {
+            var values = typeof(T)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(p => p.GetCustomAttribute(typeof(FieldDB)) != null)
+                .Select(p =>
+                    (p.GetValue(item) is string) ? $"'{p.GetValue(item)}'" : p.GetValue(item)?.ToString() ?? "");
+
+            string sqlExpression = $"INSERT INTO [dbo].[{TableName}] VALUES ({string.Join(',', values)})";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Update<T>(int id, T item)
+        {
+            var changes = typeof(T)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(p => p.GetCustomAttribute(typeof(FieldDB)) != null)
+                .Select(p =>
+                    $"{p.Name.ToLower()} = {((p.GetValue(item) is string) ? $"'{p.GetValue(item)}'" : p.GetValue(item)?.ToString() ?? "")}");
+
+            string sqlExpression = $"UPDATE [dbo].[{TableName}] SET {string.Join(',', changes)} WHERE id={id}";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            string sqlExpression = $"DELETE FROM [dbo].[{TableName}] WHERE id={id}";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private object[] GetValues(SqlDataReader reader)
+        {
+            var values = new List<object>();
+
+            for (int i = 0; i < reader.FieldCount; i++)
+                values.Add(reader.GetValue(i));
+
+            return values.ToArray();
+        }
+    }
+}
